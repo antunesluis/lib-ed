07d3856166eac28cda7525e14731f16fbf7666c1
Luis <lfantunes@inf.ufsm.br>
2023-11-21 15:40:48 -0300

    adicionando testes e funções intermediárias restantes

--- array-stack/include/array_stack.h
+++ array-stack/include/array_stack.h
@@ -2,17 +2,23 @@
 #define ARRAY_STACK_H
 
 #include <stdbool.h>
+#include <stddef.h>
+
 
 typedef struct _static_stack StaticStack;
 
-StaticStack* StaticStack_create(size_t capacity);
+StaticStack* StaticStack_create(long capacity);
 void StaticStack_destroy(StaticStack** S_ref);
 
 bool StaticStack_is_empty(const StaticStack* S);
 bool StaticStack_is_full(const StaticStack* S);
 
 void StaticStack_push(StaticStack* S, int val);
-void StaticStack_peek(const StaticStack* S);
-void StaticStack_pop(StaticStack* S);
+int StaticStack_peek(const StaticStack* S);
+int StaticStack_pop(StaticStack* S);
+
+void StaticStack_print(const StaticStack* S);
+long StaticStack_size(const StaticStack* S);
+
 
 #endif
